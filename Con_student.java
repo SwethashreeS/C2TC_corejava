94006ec5deb23a1a01d83a6b4883529e0d4cc996
Swetha shree S <swethashrees22@gmail.com>
2022-09-18 17:38:19 +0530

    initial commit
    assignment 2

---
+++ Con_student.java
@@ -0,0 +1,15 @@
+package Com.tns.assignments;
+
+public class Con_student {
+	Con_student()
+	{
+		System.out.println("Student object is created ");
+		
+	}
+public static void main(String[] args)
+{
+	Con_student obj=new Con_student();
+	
+}
+	
+}
